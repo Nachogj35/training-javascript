41b1b8b6faa7aab1dea3cff0d8c85a04e8ef5efb
nachogj35 <nachogj20@gmail.com>
2024-02-13 20:14:41 +0100

    ejemplos

--- js/app.js
+++ js/app.js
@@ -249,3 +249,43 @@
  //       alert("Please ,enter valid number");
 //}
 //}while(totalBet > 5000 || isNaN(totalBet));
+
+// interactive menu for hex colors
+
+//let option = null;
+//let colors =["red","green","blue"];
+
+//do {
+  //console.log("please ,choose your color");
+  //for(let index = 0 ; index < colors.length ; index++)
+  //{
+    //console.log(index + 1 + " . "+colors[index]);
+    
+  //}
+  
+  //console.log("Press 0 to leave")
+  //option = prompt("Make your choice");
+  //switch(option){
+
+    //case "1":
+    //console.log("%cHex code for red : #FF0000","color: red;");
+    //break;
+
+    //case "2":
+    //console.log("%cHex code for green :#008000","color: green;");
+    //break;
+
+    //case "3":
+    //console.log("%cHex code for blue : #0000FF","color: blue;");
+    //break;
+      
+    //default:
+     // if(option != 0){
+      //console.warn("Not a valid option , please try Again");//aparece el mensaje en amarillo 
+      //}else{
+      //console.log("Exiting, see you soon!");
+      //}
+    //break;
+
+  //}
+//} while (option != 0 && option != null);
